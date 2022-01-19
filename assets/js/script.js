cf6b969f8d9bc0615ffadcefe57944d25428923d
Tulsi Dey <deytulsi18@gmail.com>
2022-01-19 20:08:37 +0530

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js');
+    }
+});
+
+window.addEventListener('beforeinstallprompt', e => {
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        return e.preventDefault();
+    } else {
+        const btn = document.querySelector('#install')
+        btn.hidden = false;
+        btn.onclick = _ => e.prompt();
+        return e.preventDefault();
+    }
+});
