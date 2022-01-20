96bd0e0cec13ed29135693581c814b2548f9d291
Tulsi Dey <deytulsi18@gmail.com>
2022-01-20 18:49:24 +0530

    added dev-1 branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,41 @@
-window.addEventListener('load', () => {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js');
+
+// Install Button Javascipt
+
+let deferredPrompt;
+const installBtnContainer = document.querySelector(".install-btn-container");
+const installBtn = document.querySelector('#install');
+
+window.addEventListener('DOMContentLoaded', (event) => {    
+    window.matchMedia('(display-mode: standalone)').addEventListener('change', ({ matches }) => {
+        if (matches) {
+            installBtnContainer.style.display = "none";
+        } else {
+            installBtnContainer.style.display = "block";
+        }
+    });
+});
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    installBtnContainer.style.display = "block";
+    deferredPrompt = e;
+});
+
+installBtn.addEventListener('click', async () => {
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null;
+        }
+    } else {
+        alert('App is already installed!');
     }
 });
 
-window.addEventListener('beforeinstallprompt', e => {
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-        return e.preventDefault();
-    } else {
-        const btn = document.querySelector('#install')
-        btn.hidden = false;
-        btn.style.display = "flex";
-        btn.onclick = _ => e.prompt();
-        return e.preventDefault();
+// Register service worker
+
+window.addEventListener('load', () => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js');
     }
 });
